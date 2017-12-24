46439a0038d404ef7addee9c8b69241543fa290a
AswinAK <aswin.sct@gmail.com>
2017-12-24 05:46:52 +0530

    added delete and update

--- mongodb_test.js
+++ mongodb_test.js
@@ -9,16 +9,16 @@ MongoClient.connect('mongodb://localhost:27017', (err, database) => {
   var db = database.db('TodoApp')
 
 // INSERT TODO
-//   db.collection('Todos').insertOne({
-//     text: 'This is really new!',
-//     completed: false
-//   }, 
-//   (err, result) => {
-//     if (err) {
-//       return console.log('Unable to insert todo', err);
-//     }
-//     console.log(JSON.stringify(result.ops, undefined, 2));
-//   });
+  db.collection('Todos').insertOne({
+    text: 'More stuff to do',
+    completed: false
+  }, 
+  (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
 
 // INSERT USER
@@ -33,6 +33,8 @@ MongoClient.connect('mongodb://localhost:27017', (err, database) => {
 //     console.log(JSON.stringify(result.ops, undefined, 2));
 //   });
 
+
+  // GET COUNT OF RECORDS
   db.collection('Users').count().then((count)=>{
     console.log('count of users: ',count);
   
@@ -40,12 +42,64 @@ MongoClient.connect('mongodb://localhost:27017', (err, database) => {
     console.log('ERROR while accessing count')
   })
 
-  db.collection('Users').find().toArray().then((res)=>{
+
+  //GET  RECORDS WITH NAME
+  db.collection('Users').find({name: 'Jojo'}).toArray().then((res)=>{
       console.log('RESULTS for USERS: ');
       console.log(JSON.stringify(res,undefined,2));
   },(err)=>{
       console.log('ERROR while getting users')
   })
- 
+
+
+  // GET COUNT OF RECORDS
+  db.collection('Todos').count().then((count)=>{
+    console.log('count of Todos: ',count);
+  
+  },(err)=>{
+    console.log('ERROR while accessing count')
+  })
+
+  // GET RECORDS
+  db.collection('Todos').find().toArray().then((res)=>{
+      console.log('RESULTS for Todos: ');
+      console.log(JSON.stringify(res,undefined,2));
+  },(err)=>{
+      console.log('ERROR while getting Todos')
+  });
+
+
+  // db.collection('Todos').deleteOne({text:'Something to do'}).then((res)=>{
+  //   console.log('deleteOne: '+res);
+  // },
+  // (err)=>{
+  //   console.log('ERROR WHILE DELETING...');
+  // });
+
+  // db.collection('Todos').deleteOne({text:'Something to do'}).then((res)=>{
+  //   console.log('deleteOne: '+res);
+  // },
+  // (err)=>{
+  //   console.log('ERROR WHILE DELETING...');
+  // });
+
+  // db.collection('Todos').findOneAndDelete({text:'This is really new!'}).then((res)=>{
+  //   console.log('findOneAndDelete: '+JSON.stringify(res,undefined,2));
+  // },
+  // (err)=>{
+  //   console.log('ERROR WHILE DELETING...');
+  // });
+
+
+  //UPDATE
+  db.collection('Todos').findOneAndUpdate({text:'More stuff to do'},
+    {$set:{completed:true}},
+    {returnOriginal:false}).then((res)=>{
+      console.log('findOneAndUpdate: '+res);
+    },
+    (err)=>{
+      console.log('ERR: '+err);
+    });
+
   database.close();
 });
